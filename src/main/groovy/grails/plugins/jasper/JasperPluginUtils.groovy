d7edf3948840c61c1d2d843438dc9c946c57309b
daraii <daraii.tech@gmail.com>
2022-08-28 15:25:43 -0400

    Create utils class.

---
+++ src/main/groovy/grails/plugins/jasper/JasperPluginUtils.groovy
@@ -0,0 +1,6 @@
+package grails.plugins.jasper
+
+class JasperPluginUtils {
+    public static final String REPORT_DIRECTORY_PROP = "jasper.dir.reports"
+    public static final String TEMP_COMPILE_SUBDIRECTORY = "/.grails/.jasper"
+}
