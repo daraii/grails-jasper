83eca6b77d691fab72cd4efc56ed2b4d5860272b
daraii <daraii.tech@gmail.com>
2022-08-28 15:25:21 -0400

    Add integration tests.

---
+++ src/test/groovy/grails/plugins/jasper/JasperReportDefIntegrationSpec.groovy
@@ -0,0 +1,81 @@
+package grails.plugins.jasper
+
+import grails.testing.mixin.integration.Integration
+import grails.util.Holders
+import org.springframework.core.io.Resource
+import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+
+import static grails.plugins.jasper.JasperPluginUtils.TEMP_COMPILE_SUBDIRECTORY
+
+@Integration
+class JasperReportDefIntegrationSpec extends Specification {
+    void "Get sample report from classpath using the default location"() {
+        when:
+        def report = new JasperReportDef(name: "sample-jasper-plugin")
+
+        then:
+        report.getReport().exists()
+    }
+
+    void "Set report path"() {
+        when:
+        def directory = "classpath:/public/reports/"
+        def file = "sample-jasper-plugin"
+        def ext = "jrxml"
+        def report = new JasperReportDef()
+        report.setFilePath(directory + file + "." + ext)
+
+        then:
+        report.folder == directory
+        report.name == file
+    }
+
+    void "Get sample report with configured directory"() {
+        when:
+        grails.util.Holders.grailsApplication.config.put(JasperPluginUtils.REPORT_DIRECTORY_PROP, "classpath:/public/test/reports/")
+        def report = new JasperReportDef(name: "sample-jasper-plugin")
+
+        then:
+        report.getReport().exists()
+    }
+
+    void "Get sample report with file path"() {
+        when:
+        String userHomeDir = System.getProperty('user.home')
+        File tempFolder = new File(userHomeDir, TEMP_COMPILE_SUBDIRECTORY)
+        def fileName = "sample-jasper-plugin.jrxml"
+        if (!tempFolder.exists()) {
+            tempFolder.mkdirs()
+        }
+        def sampleReportPath = "classpath:/public/test/reports/sample-jasper-plugin.jrxml"
+        def sampleReportResource = Holders.grailsApplication.mainContext.getResource(sampleReportPath)
+        def newSampleReportFile = new File(userHomeDir, TEMP_COMPILE_SUBDIRECTORY + File.separator + fileName)
+        Files.copy(Paths.get(sampleReportResource.getURI()), Paths.get(newSampleReportFile.toURI()), StandardCopyOption.REPLACE_EXISTING)
+
+        def directoryPath = "file:" + userHomeDir + TEMP_COMPILE_SUBDIRECTORY
+        def report = new JasperReportDef(name: "sample-jasper-plugin", folder: directoryPath)
+
+        then:
+        report.getReport().exists()
+
+        cleanup:
+        doCleanup(newSampleReportFile)
+    }
+
+    private void doCleanup(report) {
+        if (report) {
+            if (report.exists()) {
+                if (report instanceof Resource) {
+                    report.getFile().delete()
+                }
+                else if(report instanceof File){
+                    report.delete()
+                }
+            }
+        }
+    }
+}
---
+++ src/test/groovy/grails/plugins/jasper/JasperServiceIntegrationSpec.groovy
@@ -0,0 +1,33 @@
+package grails.plugins.jasper
+
+import grails.testing.mixin.integration.Integration
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+import test.ExamplePersonForReport
+
+@Integration
+class JasperServiceIntegrationSpec extends Specification {
+    @Autowired
+    JasperService jasperService
+
+    void "Generate PDF report with custom configurations"(){
+        when:
+        def jasperReportDefParameters =  [:]
+        def model = [:]
+        def user = System.getProperty("user.name")
+        model.data = [new ExamplePersonForReport(name: 'Amy', email: 'amy@example.com'),
+                new ExamplePersonForReport(name: 'Brad', email: 'brad@example.com'),
+                new ExamplePersonForReport(name: 'Charlie', email: 'charlie@example.com')]
+        jasperReportDefParameters._format = "PDF"
+        jasperReportDefParameters._file = "w_iReport"
+        jasperReportDefParameters.metadataAuthor = user
+        JasperReportDef reportDef = jasperService.buildReportDefinition(jasperReportDefParameters, Locale.ENGLISH, model)
+
+        then:
+        reportDef.contentStream != null
+        reportDef.fileFormat == JasperExportFormat.PDF_FORMAT
+        reportDef.exporterConfiguration instanceof SimplePdfExporterConfiguration
+        reportDef.exporterConfiguration.metadataAuthor == user
+    }
+}
